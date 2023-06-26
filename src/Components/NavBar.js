e044238169bf57ce1d0c4f2f0fabffcf35cf2efd
Nathan Lufuluabo <45732868+natelufuluabo@users.noreply.github.com>
2023-06-26 19:42:25 -0400

    built and styled the navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,6 +1,13 @@
+import './styles.css';
+
 const NavBar = () => {
     return(
-        <nav>This is the navbar</nav>
+        <nav className="navbar sticky-top d-flex flex-row mt-3">
+            <a className="navbar-brand website-title" href="#">Foggle-Images</a>
+            <button className="themeTogglerButton">
+                <i class="fa-solid fa-moon"></i>
+            </button>
+        </nav>
     )
 };
 
