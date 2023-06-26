748b4ee2e3f903efa5277485bbdbfc41e6a0481e
Nathan Lufuluabo <45732868+natelufuluabo@users.noreply.github.com>
2023-06-26 19:17:59 -0400

    created the navbar components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      Hello, world!
+    <div className="container">
+      <NavBar/>
     </div>
   );
 }
---
+++ src/Components/NavBar.js
@@ -0,0 +1,7 @@
+const NavBar = () => {
+    return(
+        <nav>This is the navbar</nav>
+    )
+};
+
+export default NavBar;
