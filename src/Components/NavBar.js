2607670a6451d0ea8ff732707179604295eec55f
Nathan Lufuluabo <45732868+natelufuluabo@users.noreply.github.com>
2023-06-27 18:27:34 -0400

    implemented the ability reset the search results to an empty array after clicking the logo icon. This results in removing the results that were shown on screen from the last search done by the user

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,16 +1,18 @@
 import './styles.css';
-import { appThemeAtom } from '../Utilities/atoms';
+import { appThemeAtom, searchResultsAtom } from '../Utilities/atoms';
 import { useRecoilState } from 'recoil';
 
 const NavBar = () => {
     const [appTheme, setAppTheme] = useRecoilState(appThemeAtom);
+    // eslint-disable-next-line
+    const [searchResults, setSearchResults] = useRecoilState(searchResultsAtom);
     const toggleAppTheme = () => {
         appTheme === 'light' ? setAppTheme('dark'):setAppTheme('light')
     }
     return(
         <nav className="navbar d-flex flex-row col-sm-12 col-md-12 col-lg-8 pt-3">
             {/* eslint-disable-next-line */}
-            <a id={appTheme === 'light' ? 'websiteTitle-light' : 'websiteTitle-dark'} className="navbar-brand" href="#">
+            <a onClick={() => setSearchResults([])} id={appTheme === 'light' ? 'websiteTitle-light' : 'websiteTitle-dark'} className="navbar-brand" href="#">
                 Fooggle-Images
             </a>
             <button id={appTheme === 'light' ? 'themeTogglerButton-light' : 'themeTogglerButton-dark'} onClick={toggleAppTheme}>
