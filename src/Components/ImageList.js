19d233045ed1a2644476fca844ee6736afe5d68e
Nathan Lufuluabo <45732868+natelufuluabo@users.noreply.github.com>
2023-06-27 17:48:24 -0400

    changed the layout of images on screen

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -5,14 +5,31 @@ import './styles.css';
 
 const ImageList = () => {
     const imagesFetched = useRecoilValue(searchResultsAtom);
-    const imagesRendering = imagesFetched.map((image) => (
+    const firstCol = imagesFetched.slice(0, 5);
+    const secondCol = imagesFetched.slice(5);
+    const firstColRendering = firstCol.map((image) => (
         <ImageShow 
             imgSrc={image.urls.regular}
             altText={image.alt_description}
         />
-    ))
+    ));
+    const secondColRendering = secondCol.map((image) => (
+        <ImageShow 
+            imgSrc={image.urls.regular}
+            altText={image.alt_description}
+        />
+    ));
+    const imagesRenderingFunc = () => {
+        return (
+            <>
+                <div className="col-6 d-flex flex-column align-items-end gap-3">{firstColRendering}</div>
+                <div className="col-6 d-flex flex-column align-items-start gap-3">{secondColRendering}</div>
+            </>
+        )
+    }
+    const imagesRendering = imagesRenderingFunc();
     return (
-        <div className="col-sm-12 col-md-12 col-lg-8 d-flex flex-lg-row flex-md-column flex-sm-column flex-wrap gap-3 justify-content-center imagesContainer">
+        <div className="col-sm-12 col-md-12 col-lg-8 d-flex flex-row gap-3 justify-content-center imagesContainer">
            {imagesRendering}
         </div>
     )
--- src/Components/ImageShow.js
+++ src/Components/ImageShow.js
@@ -2,7 +2,7 @@ import './styles.css';
 
 const ImageShow = ({ imgSrc, altText }) => {
     return (
-        <div className="col-lg-4 col-md-12 col-sm-12 imageBox">
+        <div className="col-lg-12 col-md-12 col-sm-12 imageBox">
             <img width="100%" height="auto" src={imgSrc} alt={altText} />
         </div>
     )
